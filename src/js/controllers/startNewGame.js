26477c70cbba4f666249e522128fa98ff525c8cc
Álvaro Madrid Lara <alvaro.madrid.lara.contractor@bbva.com>
2019-08-31 21:18:01 +0200

    feat(*.js): reset result-text on start new game

--- src/js/controllers/startNewGame.js
+++ src/js/controllers/startNewGame.js
@@ -1,7 +1,10 @@
 import { clearScore } from '../lib/options.js';
+import { changeTextFirstRow, changeTextSecondRow } from '../views/resultText.js';
 
 const startNewGame = () => {
   clearScore();
+  changeTextFirstRow(resultTextInitialMessage);
+  changeTextSecondRow('');
   console.log('NEW GAME STARTED');
 }
 
