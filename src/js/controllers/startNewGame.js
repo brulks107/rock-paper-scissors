6b80503d2ebc21a0cf842cc77f0706de2f52e09f
Alvaro Madrid <alvaro.madrid@securitasdirect.es>
2019-11-19 16:08:46 +0100

    fix(*.js): fix malformed require

--- src/js/controllers/startNewGame.js
+++ src/js/controllers/startNewGame.js
@@ -1,6 +1,6 @@
 import { clearScore } from '../lib/options.js';
 import { changeTextFirstRow, changeTextSecondRow } from '../views/resultText.js';
-import updateScore from '../views/score.js';
+import updateScore from '../views/updateScore.js';
 
 const startNewGame = () => {
   clearScore();
