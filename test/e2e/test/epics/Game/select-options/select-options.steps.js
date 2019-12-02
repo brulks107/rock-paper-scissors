49bc0b291a757cac950fd3bc0f9f822a9901f154
Alvaro Madrid <alvaro.madrid@securitasdirect.es>
2019-12-02 16:14:16 +0100

    test(e2e): test added

---
+++ test/e2e/test/epics/Game/select-options/select-options.steps.js
@@ -0,0 +1,28 @@
+import { Given, When, Then, Before, After } from 'cypress-cucumber-preprocessor/steps';
+
+Given(/^game started$/, () => {
+  cy.visit('index.html');
+  cy.url().should('be.equal', 'http://localhost:8081/index.html');
+});
+
+Given(/^computer select an option (.+)$/, computeroption => {
+  cy.window().then((window) => {
+    return window.computer.computerChoice = () => computeroption
+  });
+});
+
+When(/^player selects an option (.+)$/, option => {
+  cy.get(option).click();
+});
+
+Then(/^the result show the player option (.+)$/, optionresult => {
+  cy.get('#result-text p:nth-of-type(1)').should('contains.text', `${optionresult} -`);
+});
+
+Then(/^the result show the options selected (.+)$/, result => {
+  cy.get('#result-text p:nth-of-type(1)').should('have.text', result);
+});
+
+Then(/^the result game is (.+)$/, resultgame => {
+  cy.get('#result-text p:nth-of-type(2)').should('have.text', resultgame)
+});
