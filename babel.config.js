08fa9488285d95060861913c2e522f637dd7fa6f
Alvaro Madrid <alvaro.madrid@securitasdirect.es>
2019-12-04 17:14:17 +0100

    chore(*): add @babel/preset-env preset

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,22 @@ module.exports = function (api) {
   api.cache(true);
 
   const presets = [
-    ['minify', {
-      removeConsole: {
-        'exclude': ['error', 'warn'],
-      },
-    }],
+    [
+      'minify',
+      {
+        removeConsole: {
+          'exclude': ['error', 'warn'],
+        },
+      }
+    ],
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      }
+    ],
   ];
 
   return {
