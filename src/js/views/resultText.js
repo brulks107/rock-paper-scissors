3b5399f4c5efc4d536dad4869c625c8e9aa0ce57
Alvaro Madrid <alvaro.madrid@securitasdirect.es>
2019-12-06 14:26:36 +0100

    refactor(views/resultText): code into functions

--- src/js/views/resultText.js
+++ src/js/views/resultText.js
@@ -1,15 +1,15 @@
-const resultTextRows = document.querySelectorAll('#result-text p');
+const resultTextRows = () => document.querySelectorAll('#result-text p');
 
-const resultTextRow = row => resultTextRows[row - 1];
+const resultTextRow = row => resultTextRows()[row - 1];
 
 export const changeTextFirstRow = text => {
-  resultTextRow(1).innerHTML = text;
+  return resultTextRow(1).innerHTML = text;
 };
 
 export const changeTextSecondRow = text => {
-  resultTextRow(2).innerHTML = text;
+  return resultTextRow(2).innerHTML = text;
 };
 
 export const changeToResultColor = color => {
-  resultTextRow(2).style.color = color;
+  return resultTextRow(2).style.color = color;
 };
