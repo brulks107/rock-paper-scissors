8a9347f8d29f8530305585543ad8e0af38b77805
amadridlar <amadridlar@gmail.com>
2019-12-14 22:41:09 +0100

    test(e2e/end-of-game): step implementation [FAT-18]

--- test/e2e/test/epics/Game/end-of-game/end-of-game.steps.js
+++ test/e2e/test/epics/Game/end-of-game/end-of-game.steps.js
@@ -1,6 +1,6 @@
 import { When, Then, And } from 'cypress-cucumber-preprocessor/steps';
 
-When(/^(.+) wins the game$/, function (player) {
+When(/^(.+) wins the game$/, player => {
   const winPlayer = {
     user: () => {
       cy.window().then((window) => {
@@ -21,10 +21,10 @@ When(/^(.+) wins the game$/, function (player) {
   cy.get('#rock').click();
 });
 
-Then(/^the end of game text is shown$/, function () {
-  return 'pending';
+Then(/^the end of game text is shown$/, () => {
+  cy.get('#result-text > p:nth-of-type(1)').should('have.text', 'End of game');
 });
 
-And(/^the text shows who wins with the text \"([^\"]*)\"$/, function (text) {
-  return 'pending';
+And(/^the text shows who wins with the text (.+)$/, text => {
+  cy.get('#result-text > p:nth-of-type(2)').should('have.text', text);
 });
